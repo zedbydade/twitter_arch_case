ff004eb28582ec88f632ba085898a85c60f955d2
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-05-27 14:38:32 -0300

    feat: Add cache solution

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,4 +6,14 @@ class TweetsController < ApplicationController
     tweets = Tweet.where(sender_id: follower_ids)
     render json: tweets.to_json, status: 200
   end
+
+  def create 
+    sender_id = params[:sender_id] 
+    body = params[:body]
+    tweet = Tweet.create!(sender_id: sender_id, body: body)
+    Follow.where(follower_id: sender_id).each do |follow| 
+      Rails.cache.write(follow.followee_id, tweet)
+    end
+    render json: tweet, status: 200 
+  end
 end
